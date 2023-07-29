497f9aac7dbc3105eea17500a8eee3f3ddb63137
Aaron Alexander <ajalexander.dev@gmail.com>
2023-07-29 17:57:26 -0400

    Add the langchain text splitter to the /split endpoint

--- api/backend/handlers/chunk_text.py
+++ api/backend/handlers/chunk_text.py
@@ -1,5 +1,7 @@
 from backend.request_models import ChunkTextRequestDto
+from langchain.text_splitter import CharacterTextSplitter
 
 
-def chunk_text_handler(input_text: ChunkTextRequestDto):
-    return [letter for letter in input_text.text]
+def chunk_text_handler(data: ChunkTextRequestDto):
+    text_splitter = CharacterTextSplitter()
+    return text_splitter.split_text(data.text)
