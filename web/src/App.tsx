cc65c7052322795bb00183f7ce1bac0bc0887698
Aaron Alexander <ajalexander.dev@gmail.com>
2023-07-29 10:06:00 -0400

    Add a VS Code settings file to move to 4 spaces for tabs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import RecordForSpeech from "./RecordForSpeech";
 
 function App() {
-  return (
-    <div className="App">
-      <RecordForSpeech />
-    </div>
-  );
+    return (
+        <div className="App">
+            <RecordForSpeech />
+        </div>
+    );
 }
 
 export default App;
