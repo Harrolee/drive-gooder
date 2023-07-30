3ef81faab2b01fb5ce43185ad9fc3743d1224e91
Aaron Alexander <ajalexander.dev@gmail.com>
2023-07-30 13:16:54 -0400

    Start of custom audio controls

--- web/src/component/AudioPlayerControls.tsx
+++ web/src/component/AudioPlayerControls.tsx
@@ -1,9 +1,21 @@
+import { useCallback, useRef } from "react";
+
 interface AudioPlayerControlsProps {
-  src: string;
+    src: string;
 }
 
 export function AudioPlayerControls(props: AudioPlayerControlsProps) {
-  return <audio
-    src={props.src}
-    controls />;
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const handlePlay = useCallback(() => {
+        console.log("Staring to play");
+        audioRef.current?.play();
+    }, []);
+
+    return <div>
+        <audio
+            src={props.src}
+            ref={audioRef} />
+        <button onClick={handlePlay}>Play</button>
+    </div>;
 }
