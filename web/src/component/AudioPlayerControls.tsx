c3c7abc900ae9ac58ead79975cf395c4ead60bd3
Nate White <natewhite1999@gmail.com>
2023-07-30 14:42:47 -0400

    play pause added

--- web/src/component/AudioPlayerControls.tsx
+++ web/src/component/AudioPlayerControls.tsx
@@ -4,18 +4,24 @@ interface AudioPlayerControlsProps {
     src: string;
 }
 
-export function AudioPlayerControls(props: AudioPlayerControlsProps) {
+export function AudioPlayerControls(this: any, props: AudioPlayerControlsProps) {
     const audioRef = useRef<HTMLAudioElement>(null);
-
     const handlePlay = useCallback(() => {
         console.log("Staring to play");
         audioRef.current?.play();
     }, []);
+    const handlePause = useCallback(() => {
+      audioRef.current?.pause();
+  }, []);
+
 
     return <div>
         <audio
+            id = "audio"
             src={props.src}
-            ref={audioRef} />
+            ref={audioRef}/>
         <button onClick={handlePlay}>Play</button>
+        <button onClick={handlePause}>Pause</button>
     </div>;
 }
+export default AudioPlayerControls
