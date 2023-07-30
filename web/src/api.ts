03adf5652f4455c67bc97f3da9464a8d721c7a63
Lee Harrold <lhharrold@sep.com>
2023-07-30 14:52:48 -0400

    Use fetch for Ask

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,139 +1,149 @@
 import axios from "axios";
 
 const credentials = {
-    username: "",
-    password: "",
+  username: "",
+  password: "",
 };
 
 export const storeLoginCredentials = (username: string, password: string) => {
-    credentials.username = username;
-    credentials.password = password;
+  credentials.username = username;
+  credentials.password = password;
 };
 
 const buildAuthorizationHeaderFromStoredCredentials = () => {
-    return buildAuthorizationHeader(credentials.username, credentials.password);
+  return buildAuthorizationHeader(credentials.username, credentials.password);
 };
 
 const buildAuthorizationHeader = (username: string, password: string) => {
-    const data = btoa(`${username}:${password}`);
-    return `Basic ${data}`;
+  const data = btoa(`${username}:${password}`);
+  return `Basic ${data}`;
 };
 
 export const authenticate = async (username: string, password: string) => {
-    return axios.post(`${process.env.REACT_APP_API_ROOT}/authenticate`, null, {
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Authorization": buildAuthorizationHeader(username, password)
-        },
-    }).then(() => {
-        return true;
+  return axios
+    .post(`${process.env.REACT_APP_API_ROOT}/authenticate`, null, {
+      headers: {
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Authorization: buildAuthorizationHeader(username, password),
+      },
+    })
+    .then(() => {
+      return true;
     })
     .catch((error) => {
-        console.error(error);
-        return false;
+      console.error(error);
+      return false;
     });
-}
-
+};
 
 export const getSplit = async (text: string): Promise<string[]> => {
-    const data = {
-        text: text,
-    };
+  const data = {
+    text: text,
+  };
 
-    return fetch(`${process.env.REACT_APP_API_ROOT}/split`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Authorization": buildAuthorizationHeaderFromStoredCredentials(),
-            "responseType": "blob",
-        },
-        body: JSON.stringify(data),
-    })
+  return fetch(`${process.env.REACT_APP_API_ROOT}/split`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: buildAuthorizationHeaderFromStoredCredentials(),
+      responseType: "blob",
+    },
+    body: JSON.stringify(data),
+  })
     .then(async (response) => {
-        return (await response.json()) as string[];
+      return (await response.json()) as string[];
     })
     .catch((error) => {
-        console.error(error);
-        return Promise.reject();
+      console.error(error);
+      return Promise.reject();
     });
-}
+};
 
 export const readText = (text: string, emotion: string, speed: number) => {
-    const data = {
-        text,
-        emotion,
-        speed,
-    };
+  const data = {
+    text,
+    emotion,
+    speed,
+  };
 
-    return fetch(`${process.env.REACT_APP_API_ROOT}/read`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Authorization": buildAuthorizationHeaderFromStoredCredentials(),
-            "responseType": "blob",
-        },
-        body: JSON.stringify(data),
-    })
+  return fetch(`${process.env.REACT_APP_API_ROOT}/read`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: buildAuthorizationHeaderFromStoredCredentials(),
+      responseType: "blob",
+    },
+    body: JSON.stringify(data),
+  })
     .then(async (response) => {
-        return await response.blob();
+      return await response.blob();
     })
     .catch((error) => {
-        console.error(error);
-        return Promise.reject();
+      console.error(error);
+      return Promise.reject();
     });
-}
+};
 
-export const ask = (audio: Blob, text: string, emotion: string, speed: number) => {
-    const data = {
-        text,
-        emotion,
-        speed,
-    };
-    const formData = new FormData();
-    formData.append("question.wav", audio);
-    formData.append("text", text);
-    formData.append("emotion", emotion);
-    formData.append("speed", speed.toString());
-    return axios.post(`${process.env.REACT_APP_API_ROOT}/ask`, data, {
-        headers: {
-            "content-type": "multipart/form-data",
-            "Access-Control-Allow-Origin": "*",
-            "Authorization": buildAuthorizationHeaderFromStoredCredentials(),
-            "responseType": "blob",
-        },
-    }).then((response) => {
-        return response.data;
-    })
-        .catch((error) => {
-            console.error(error);
-        });
-}
+export const ask = (
+  audio: Blob,
+  text: string,
+  emotion: string,
+  speed: number
+) => {
+  const formData = new FormData();
+  formData.append("question.wav", JSON.stringify(audio));
+  formData.append("text", text);
+  formData.append("emotion", emotion);
+  formData.append("speed", speed.toString());
 
-export const summarize = (text: string, emotion: string, speed: number): Promise<Blob> => {
-    const data = {
-        text,
-        emotion,
-        speed,
-    };
-
-    return fetch(`${process.env.REACT_APP_API_ROOT}/summarize`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Authorization": buildAuthorizationHeaderFromStoredCredentials(),
-            "responseType": "blob",
-        },
-        body: JSON.stringify(data),
-    })
+  return fetch(`${process.env.REACT_APP_API_ROOT}/ask`, {
+    method: "POST",
+    headers: {
+      "content-type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: buildAuthorizationHeaderFromStoredCredentials(),
+      responseType: "blob",
+    },
+    body: formData,
+  })
     .then(async (response) => {
-        return await response.blob();
+      return await response.blob();
     })
     .catch((error) => {
-        console.error(error);
-        return Promise.reject();
+      console.error(error);
+      return Promise.reject();
     });
-}
+};
+
+export const summarize = (
+  text: string,
+  emotion: string,
+  speed: number
+): Promise<Blob> => {
+  const data = {
+    text,
+    emotion,
+    speed,
+  };
+
+  return fetch(`${process.env.REACT_APP_API_ROOT}/summarize`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: buildAuthorizationHeaderFromStoredCredentials(),
+      responseType: "blob",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async (response) => {
+      return await response.blob();
+    })
+    .catch((error) => {
+      console.error(error);
+      return Promise.reject();
+    });
+};
