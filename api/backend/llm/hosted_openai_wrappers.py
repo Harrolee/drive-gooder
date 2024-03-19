5c423697a897d10dff908d0a2a1e30a20380736f
Lee Harrold <lhharrold@sep.com>
2024-03-19 14:05:12 -0400

    stable build state

--- api/backend/llm/hosted_openai_wrappers.py
+++ api/backend/llm/hosted_openai_wrappers.py
@@ -1,9 +1,11 @@
-from langchain_openai import OpenAI
+from langchain_openai import OpenAI as langchain_openai
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
 from langchain.chains.question_answering import load_qa_chain
-import openai
+from openai import OpenAI
+
+openai_client = OpenAI()
 
 def summarize(text: str):
     text_splitter = CharacterTextSplitter()
@@ -11,7 +13,7 @@ def summarize(text: str):
 
     docs = [Document(page_content=t) for t in split_texts]
 
-    llm = OpenAI(temperature=0, model="gpt-3.5-turbo")
+    llm = langchain_openai(temperature=0, model="gpt-3.5-turbo-instruct")
 
     chain = load_summarize_chain(llm, chain_type="map_reduce")
 
@@ -20,11 +22,11 @@ def summarize(text: str):
     return summary
 
 def speech_to_text(data: object):
-    transcript = openai.Audio.translate("whisper-1", data)
+    transcript = openai_client.audio.translate("whisper-1", data)
     return transcript
 
 def ask_question(question: str, text: str):
-    chain = load_qa_chain(OpenAI(temperature=0, model="gpt-3.5-turbo"), chain_type="stuff")
+    chain = load_qa_chain(langchain_openai(temperature=0, model="gpt-3.5-turbo"), chain_type="stuff")
 
     text_splitter = CharacterTextSplitter()
     docs = text_splitter.create_documents(texts = [text])
